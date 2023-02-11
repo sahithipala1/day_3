ecb4d63079952fbb2f4dd60d26b1e94a1c5e23e7
Sahithi Pala <43152542+sahithipala1@users.noreply.github.com>
2023-02-11 15:28:44 +0100

    day 3 python

---
+++ new.py
@@ -0,0 +1,7 @@
+# recipe of tiramisu
+
+food = input("Name a food : ")
+ingredients = input("Please make list  of the recipe: ")
+cooking = input("way to cook: ")
+
+print(food, "is really yummy recipe and ", ingredients, "are used in the recipe", ". The way of cooking is ", cooking)
